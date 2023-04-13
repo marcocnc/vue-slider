baaeca5ffca493e1846a7fae611d9b8271f7627a
marcocnc <marcocancelliere56@gmail.com>
2023-04-13 15:26:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Vue Slider
+===
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+
+
+## Procedimento
+1. Dopo aver ricopiato il markup dell'esercizio Array Carousel, nel file script si richiama Vue;
+2. Nel data, si crea un array di images che avrà le src delle varie immagini, e si crea un contatore uguale a zero;
+
---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            images: [
+                'assets/img/1.webp',
+                'assets/img/2.webp',
+                'assets/img/3.jpg',
+                'assets/img/4.jpg',
+                'assets/img/5.jpg'
+            ],
+            counter: 0
+        }
+    }
+}).mount('#app')
+
+
+                
